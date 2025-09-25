adf936583a05df2f88dd774f3532441f7df5e51e
Atharva Keluskar <keluskaratharva999@gmail.com>
2025-09-25 20:31:52 +0530

    Updated backend with correct environment variable usage

--- index.mjs
+++ index.mjs
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
